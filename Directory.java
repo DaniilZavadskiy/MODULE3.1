6d47b265631f769a1fe6f97e204f10166ea03225
DaniilZavadskiy <danilzavadskiy63@gmail.com>
2016-04-29 18:37:58 +0300

    Added files via upload

---
+++ Directory.java
@@ -0,0 +1,15 @@
+package com.goit.gojavaonline;
+
+import java.util.List;
+
+public class Directory {
+    public List<com.goit.gojavaonline.File> getFile() {
+        return File;
+    }
+
+    public void setFile(List<com.goit.gojavaonline.File> file) {
+        File = file;
+    }
+
+    List<File> File;
+}
---
+++ File.java
@@ -0,0 +1,5 @@
+package com.goit.gojavaonline;
+
+
+public class File {
+}
